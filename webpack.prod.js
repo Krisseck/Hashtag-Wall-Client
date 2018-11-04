964c6601ac6bfd6a7388c0ff837be562185c88ef
Kristian Polso <kristian.polso@gmail.com>
2018-11-04 17:10:16 +0200

    clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin(['dist'])]
 });
